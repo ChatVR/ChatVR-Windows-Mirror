8cad12c283df34af6d484f79fba7c767e1dc49ed
Noor-Nasri <noor.nasri@hotmail.com>
2021-03-27 16:47:01 -0500

    x to the dddddd

--- Assets/Local Scripts/LocalPlayer.cs
+++ Assets/Local Scripts/LocalPlayer.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using UnityEngine.SpatialTracking;
 
 public class LocalPlayer : NetworkBehaviour
 {
     // Start is called before the first frame update
     //public GameObject movement;
+    public GameObject rig;
     public GameObject camera;
+    public GameObject rightArm;
+    public GameObject leftArm;
+
     void Start()
     {
         if (isLocalPlayer)
         {
             //(CameraOffset)movement.GetComponent<CameraOffset>().enabled = true;
             camera.SetActive(true);
+            ((VRPlayerController)rig.GetComponent<VRPlayerController>()).enabled = true;
+            ((TrackedPoseDriver)rightArm.GetComponent<TrackedPoseDriver>()).enabled = true;
+            ((TrackedPoseDriver)leftArm.GetComponent<TrackedPoseDriver>()).enabled = true;
         }
 
     }

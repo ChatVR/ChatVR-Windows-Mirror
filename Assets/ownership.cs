f20315bd8ff38012e7e6875e0714354795964853
Noor-Nasri <noor.nasri@hotmail.com>
2021-03-28 07:18:09 -0500

    network ownership

---
+++ Assets/ownership.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.SceneManagement;
+
+public class ownership : NetworkBehaviour
+{
+    public GameObject listenFor;
+    private GameObject currentOwner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isServer)
+        {
+            GameObject closest = null;
+            float dist = 99999999999999;
+
+            foreach (GameObject root in SceneManager.GetActiveScene().GetRootGameObjects())
+            {
+                if (root.name == "Player Model(Clone)")
+                {
+                    foreach (Transform rig in root.transform)
+                    {
+                        float curDist = Vector3.Magnitude(listenFor.transform.position - rig.position);
+                        if (curDist < dist)
+                        {
+                            dist = curDist;
+                            closest = root;
+                        }
+                    }
+                    
+                }
+            }
+
+            if (!(closest == currentOwner))
+            {
+                currentOwner = closest;
+                NetworkIdentity identity = listenFor.GetComponent<NetworkIdentity>();
+                NetworkIdentity newOwner = closest.GetComponent<NetworkIdentity>();
+                identity.AssignClientAuthority(newOwner.connectionToClient); //NetworkConnection.NetworkConnectionToClient(newOwner));
+                Debug.Log("Set network authority");
+            }
+        }
+    }
+}
